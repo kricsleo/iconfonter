dd45d83f3b73b70d84696f26ff299b0252652ea9
许圣峰 <kricsleo@163.com>
2022-09-27 10:16:33 +0800

    fix: remove whitespace

--- src/ts.ts
+++ src/ts.ts
@@ -5,6 +5,6 @@ export async function writeDTS(icons: Icon[], options: DTSOptions) {
   const names = icons.map(icon => `\r\n  | '${icon.font_class}'`).join('')
   const typeName = options.name || 'IconName'
   const prefix = options.path!.endsWith('.d.ts') ? '' : 'export '
-  const content = `${prefix}type ${typeName} = ${names}` + '\r\n'
+  const content = `${prefix}type ${typeName} =${names}` + '\r\n'
   await writeFile(options.path!, content)
 }
